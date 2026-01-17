011362149553824e8e389b683d1feadfd915085c
Gareth Evans <gazz.sf@gmail.com>
2026-01-17 11:55:29 +0000

    Tweak primetools build script

---
+++ include/result.hpp
@@ -0,0 +1,10 @@
+#ifndef RESULT_HPP
+#define RESULT_HPP
+
+typedef enum FactorizationResult {
+    FactorisationSuccess = 0,
+    FactorisationFailure = 1,
+    FactorisationPartial = 2
+} FactorizationResult;
+
+#endif // RESULT_HPP
